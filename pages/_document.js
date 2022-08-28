14f76f6960fb73b8603a1b290865892900a1674a
immahesh111 <maheshkumar17032k3@gmail.com>
2022-08-28 17:19:09 +0530

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document from 'next/document';
+import Html from 'next/document';
+import Head from 'next/document';
+import Main from 'next/document';
+import NextScript from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
